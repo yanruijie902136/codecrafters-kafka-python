5f3839300c2f47f3151059918e08e3e19ccb17e8
yanruijie902136 <yanruijie902136@gmail.com>
2024-09-14 13:03:44 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -26,19 +26,30 @@ class KafkaRequest:
 
 
 def make_response(request: KafkaRequest):
+    response_header = request.correlation_id.to_bytes(4)
+
     valid_api_versions = [0, 1, 2, 3, 4]
     error_code = (
         ErrorCode.NONE
         if request.api_version in valid_api_versions
         else ErrorCode.UNSUPPORTED_VERSION
     )
-
-    response = (
-        int(10).to_bytes(4) +
-        request.correlation_id.to_bytes(4) +
-        error_code.value.to_bytes(2)
+    min_version, max_version = 0, 4
+    throttle_time_ms = 0
+    tag_buffer = b"\x00"
+    response_body = (
+        error_code.value.to_bytes(2) +
+        int(2).to_bytes(1) +
+        request.api_key.to_bytes(2) +
+        min_version.to_bytes(2) +
+        max_version.to_bytes(2) +
+        tag_buffer +
+        throttle_time_ms.to_bytes(4) +
+        tag_buffer
     )
-    return response
+
+    response_length = len(response_header) + len(response_body)
+    return int(response_length).to_bytes(4) + response_header + response_body
 
 
 def main():
