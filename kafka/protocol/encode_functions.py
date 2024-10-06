3b01c5aa6ed8fafe4c0018856ad4c29ac8c5c12c
yanruijie902136 <yanruijie902136@gmail.com>
2024-10-07 05:31:37 +0800

    codecrafters submit [skip ci]

--- kafka/protocol/encode_functions.py
+++ kafka/protocol/encode_functions.py
@@ -9,24 +9,6 @@ def encode_boolean(boolean: bool) -> bytes:
     return int(boolean).to_bytes(1)
 
 
-def encode_compact_array(array: list, encode_function: Optional[EncodeFunction] = None) -> bytes:
-    encoded_items = b"".join(
-        item.encode() if encode_function is None else encode_function(item)
-        for item in array
-    )
-    return encode_varint(len(array) + 1) + encoded_items
-
-
-def encode_compact_nullable_string(string: str) -> bytes:
-    if not string:
-        return encode_varint(0)
-    return encode_varint(len(string) + 1) + string.encode()
-
-
-def encode_compact_string(string: str) -> bytes:
-    return encode_varint(len(string) + 1) + string.encode()
-
-
 def encode_int8(integer: int) -> bytes:
     return integer.to_bytes(1, signed=True)
 
@@ -43,21 +25,48 @@ def encode_int64(integer: int) -> bytes:
     return integer.to_bytes(8, signed=True)
 
 
-def encode_tagged_fields() -> bytes:
-    return b"\x00"
-
-
-def encode_uuid(uuid: uuid.UUID) -> bytes:
-    return uuid.bytes
-
-
 def encode_varint(integer: int) -> bytes:
+    if integer < 0:
+        integer += 1 << 32
+
     BASE = 128
     encoding = b""
     while True:
         integer, n = divmod(integer, BASE)
         if integer > 0:
             n += BASE
-        encoding += encode_int8(n)
+        encoding += n.to_bytes(1)
         if integer == 0:
             return encoding
+
+
+def encode_varlong(integer: int) -> bytes:
+    if integer < 0:
+        integer += 1 << 64
+    return encode_varint(integer)
+
+
+def encode_uuid(uuid: uuid.UUID) -> bytes:
+    return uuid.bytes
+
+
+def encode_compact_string(string: str) -> bytes:
+    return encode_varint(len(string) + 1) + string.encode()
+
+
+def encode_compact_nullable_string(string: str) -> bytes:
+    if not string:
+        return encode_varint(0)
+    return encode_varint(len(string) + 1) + string.encode()
+
+
+def encode_compact_array(array: list, encode_function: Optional[EncodeFunction] = None) -> bytes:
+    encoded_items = b"".join(
+        item.encode() if encode_function is None else encode_function(item)
+        for item in array
+    )
+    return encode_varint(len(array) + 1) + encoded_items
+
+
+def encode_tagged_fields() -> bytes:
+    return b"\x00"
