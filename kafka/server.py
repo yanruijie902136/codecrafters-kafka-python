2d7e8a0539916a86f134144f551b5ddaf5a6d984
yanruijie902136 <yanruijie902136@gmail.com>
2025-03-28 17:19:57 +0800

    codecrafters submit [skip ci]

--- kafka/server.py
+++ kafka/server.py
@@ -3,6 +3,26 @@ import asyncio
 from .messages import make_response, read_request
 
 
+class KafkaClientConnection:
+    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        self._reader = reader
+        self._writer = writer
+
+    async def recv_request(self):
+        return await read_request(self._reader)
+
+    async def send_response(self, response):
+        self._writer.write(response.encode())
+        await self._writer.drain()
+
+    async def __aenter__(self):
+        pass
+
+    async def __aexit__(self, exc_type, exc, tb):
+        self._writer.close()
+        await self._writer.wait_closed()
+
+
 class KafkaServer:
     def __init__(self, host="localhost", port=9092):
         self._host = host
@@ -13,15 +33,10 @@ class KafkaServer:
         async with server:
             await server.serve_forever()
 
-    async def _client_connected_cb(self, stream_reader: asyncio.StreamReader, stream_writer: asyncio.StreamWriter):
-        try:
+    async def _client_connected_cb(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        conn = KafkaClientConnection(reader, writer)
+        async with conn:
             while True:
-                request = await read_request(stream_reader)
+                request = await conn.recv_request()
                 response = make_response(request)
-                stream_writer.write(response.encode())
-                await stream_writer.drain()
-        except asyncio.IncompleteReadError:
-            return
-        finally:
-            stream_writer.close()
-            await stream_writer.wait_closed()
+                await conn.send_response(response)
