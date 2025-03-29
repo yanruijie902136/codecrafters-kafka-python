a4da0a4f915d78adbee0213536f3a65756cd5e59
yanruijie902136 <yanruijie902136@gmail.com>
2025-03-30 06:12:04 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,30 @@
-import socket  # noqa: F401
+import asyncio
 
 
-def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+class KafkaClientConnection:
+    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+        self._reader = reader
+        self._writer = writer
 
-    # Uncomment this to pass the first stage
-    #
-    # server = socket.create_server(("localhost", 9092), reuse_port=True)
-    # server.accept() # wait for client
+    async def __aenter__(self) -> None:
+        pass
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
+        self._writer.close()
+        await self._writer.wait_closed()
+
+
+class KafkaServer:
+    async def start(self) -> None:
+        server = await asyncio.start_server(self._client_connected_cb, host="localhost", port=9092, reuse_port=True)
+        async with server:
+            await server.serve_forever()
+
+    async def _client_connected_cb(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+        connection = KafkaClientConnection(reader, writer)
+        async with connection:
+            pass
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(KafkaServer().start())
